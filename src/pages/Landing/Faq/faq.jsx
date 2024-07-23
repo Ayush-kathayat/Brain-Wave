b5a09138dd0a2d0dda8408a90b3944edc5616de1
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-23 10:05:49 +0530

    (refactor_1) : refactored the FAQ's Section again

--- src/pages/Landing/Faq/faq.jsx
+++ src/pages/Landing/Faq/faq.jsx
@@ -39,13 +39,13 @@ const Faq = () => {
       <ThemeProvider theme={theme}>
         <Accordion className="accord">
           <AccordionSummary>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Question 1: How can I upload and organize my course content?{" "}
             </Typography>
           </AccordionSummary>
 
           <AccordionDetails>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Answer: You can easily upload your course content, including
               links, images, and PDFs, through our intuitive interface. Simply
               create a new module, add your content, and categorize it to keep
@@ -56,12 +56,12 @@ const Faq = () => {
 
         <Accordion className="accord">
           <AccordionSummary>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Question 2: Is my data secure on this platform?
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Answer: Yes, your data is secure. We use industry-standard
               security practices and robust authentication mechanisms to ensure
               that only you can access and modify your content. All data is
@@ -72,12 +72,12 @@ const Faq = () => {
 
         <Accordion className="accord">
           <AccordionSummary>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Question 3: Can I access my content from multiple devices?
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Answer: Absolutely! Our platform supports cross-device
               synchronization, so you can access your content from any device,
               anywhere. Any changes you make will be reflected across all your
@@ -88,12 +88,12 @@ const Faq = () => {
 
         <Accordion className="accord">
           <AccordionSummary>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Question 4: What happens if I forget my password?
             </Typography>
           </AccordionSummary>
           <AccordionDetails>
-            <Typography sx={{ fontSize: "1.5rem" }}>
+            <Typography sx={{ fontSize: "1.2rem" }}>
               Answer:If you forget your password, you can easily reset it using
               the "Forgot Password" feature on the login page. Follow the
               instructions to reset your password and regain access to your
