051702353a021d757b863e042f3f5d8e8512fdbb
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-27 21:51:24 +0530

    (home-page) : added the css for the svg nothing-here

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,9 +21,9 @@ const Home = () => {
           {/* //! the thing is here will be a component which will be used irrespective of if the data is there or not */}
 
         <div className="nothing-here-wrapper">
-          <img src="./nothing-here.svg" alt="Nothing :{" />
+          <img className="nothing-here-svg" src="./nothing-here.svg" alt="Nothing :{" />
           <h1 className="nothing-here-title">Nothing Added Here Yet</h1>
-          <p className="nothing-here-para"> Click on the [+] add button to add items in this course. </p>
+          <p className="nothing-here-para"> Click on the [ + ] add button to add items in this course. </p>
         </div>
 
 
