70bc0b39da335d693ecf0e3e19fd3d605236cfe4
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-29 00:21:37 +0530

    (addResource): only the button is built by now

---
+++ src/components/AddResources.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./addResources.css";
+
+const AddResources = () => {
+  return (
+    <>
+      <div className="btn-wrapper">
+        <div className="add-btn">
+          <span>+</span>
+          Add
+          <img src="arrow-down.svg" alt="" />
+        </div>
+
+      </div>
+    </>
+  );
+};
+
+export default AddResources;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,32 +4,38 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Components
 import Nav from "../../components/Navbar";
+import AddResources from "@/components/AddResources";
 
 import "./home.css";
 
 const Home = () => {
-
-  const [data , setData] = useState(false);
+  const [data, setData] = useState(false);
   return (
     <>
       <Nav />
 
       <div className="home-wrapper">
+        {data ? (
+          <></>
+        ) : (
+          <div className="nothing-here-container">
+            {/* //! the thing is here will be a component which will be used irrespective of if the data is there or not */}
 
-        {data ? (<></>) : (
-          <>
-          {/* //! the thing is here will be a component which will be used irrespective of if the data is there or not */}
-
-        <div className="nothing-here-wrapper">
-          <img className="nothing-here-svg" src="./nothing-here.svg" alt="Nothing :{" />
-          <h1 className="nothing-here-title">Nothing Added Here Yet</h1>
-          <p className="nothing-here-para"> Click on the [ + ] add button to add items in this course. </p>
-        </div>
-
-
-          </>
+            <AddResources />
+            <div className="nothing-here-wrapper">
+              <img
+                className="nothing-here-svg"
+                src="./nothing-here.svg"
+                alt="Nothing :{"
+              />
+              <h1 className="nothing-here-title">Nothing Added Here Yet</h1>
+              <p className="nothing-here-para">
+                {" "}
+                Click on the [ + ] add button to add items in this course.{" "}
+              </p>
+            </div>
+          </div>
         )}
-       
       </div>
     </>
   );
