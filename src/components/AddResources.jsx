ce04cedf962d36a42405e4cda6717ea37798d823
Ayush-kathayat <kathayatayush789@gmail.com>
2024-07-29 10:33:41 +0530

    (addResource): Added aos in the button

--- src/components/AddResources.jsx
+++ src/components/AddResources.jsx
@@ -5,13 +5,18 @@ import "./addResources.css";
 const AddResources = () => {
   return (
     <>
-      <div className="btn-wrapper">
+      <div
+        className="btn-wrapper"
+        data-aos="fade-down"
+        data-aos-delay="1000"
+        data-aos-duration="400"
+        data-aos-easing="ease-in-out"
+      >
         <div className="add-btn">
           <span>+</span>
           Add
           <img src="arrow-down.svg" alt="" />
         </div>
-
       </div>
     </>
   );
