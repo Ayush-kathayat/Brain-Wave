8785f8f9a03b6292b34b16bb71e81f23311b2dba
Ayush-kathayat <kathayatayush789@gmail.com>
2024-08-12 20:04:42 +0530

    (action-panel) : 90% of action panel is done now i just have to try it with the 3dots vertical icon then it will go upto 100%

--- src/components/actionPanel.jsx
+++ src/components/actionPanel.jsx
@@ -1,17 +1,37 @@
 import "./actionPanel.css";
 
-const ActionPanel = () => {
+const ActionPanel = ({handleOpenModal}) => {
   const panelData = [
-    { icon: "./folder.svg", title: "Create Module" },
-    { icon: "./link.svg", title: "Add a Link" },
-    { icon: "./upload.svg", title: "Upload" },
+    {
+      heading: "Create Module",
+      description: "Add Item to this Module",
+      icon: "./folder.svg",
+      title: "Add a Module",
+      hash_map: {
+        "Module Name": "Enter Module Name",
+      },
+    },
+    {
+      heading: "Add New Link",
+      description: "LINk",
+      icon: "./link.svg",
+      title: "Add a Link",
+      hash_map: {
+        URL: "Enter URL",
+        "Display Name": "Enter Display Name",
+      },
+    },
+    {
+      icon: "./upload.svg",
+      title: "Upload",
+    },
   ];
 
   return (
     <>
       <div className="action-panel">
         {panelData.map((data, index) => (
-          <div key={index} className="action-wrapper">
+          <div key={index} className="action-wrapper" onClick={() => handleOpenModal(data.heading , data.description , data.icon, data.hash_map)}>
             <img src={data.icon} alt="module" />
             <h1>{data.title}</h1>
           </div>
@@ -22,49 +42,6 @@ const ActionPanel = () => {
 };
 
 export default ActionPanel;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // <div className="drop-down">
 //   <div
